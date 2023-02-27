a5b3c702449cdbd46468af385e9cc317db689d0e
bhanu <bhanusharma576@gmail.com>
2023-02-27 10:34:33 +0530

    recognition is not contineous

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+//elements
+const startBtn = document.querySelector("#start");
+const stopBtn = document.querySelector("#stop");
+
+//Speech recognition setup
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+//sr start
+recognition.onstart = function (){
+    console.log("vr active");
+
+};
+
+//sr stop
+recognition.onend = function (){
+    console.log("vr end");
+};
+// speech contineous
+recognition.continuous = true;
+
+startBtn.addEventListener("click",()=>{
+    recognition.start();
+});
+stopBtn.addEventListener("click",()=>{
+    recognition.stop();
+});
