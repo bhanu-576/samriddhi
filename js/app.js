f2fcfcdaa4084a41e6f63f9e4eb16744c13cff02
bhanu <bhanusharma576@gmail.com>
2023-03-16 20:05:13 +0530

    samriddhi voice setting

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 //elements
 const startBtn = document.querySelector("#start");
 const stopBtn = document.querySelector("#stop");
+const speakBtn = document.querySelector("#speak");
+
 
 //Speech recognition setup
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -25,3 +27,19 @@ startBtn.addEventListener("click",()=>{
 stopBtn.addEventListener("click",()=>{
     recognition.stop();
 });
+//samriddhi speech
+function readOut(message){
+
+const speech = new SpeechSynthesisUtterance()
+// const allVoice = speechSynthesis.getVoices();
+speech.text = message;
+// speech.voice = allVoice[7];
+speech.volume = 1
+window.speechSynthesis.speak(speech)
+console.log('speaking out');
+}
+
+speakBtn.addEventListener("click",()=>{
+    readOut("HI,bhanu I love you");
+});
+
